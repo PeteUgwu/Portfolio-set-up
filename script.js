bcbe189c1045a43ed8c97ae513a77db06a724235
peter <peterugwu64@gmail.com>
2022-10-18 10:23:24 +0100

    Add nav-mobile class and fix linters

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let hamburger = document.querySelector(".hamburger");
-let mobileMenu = document.querySelector(".mobile-menu");
-let cancel = document.querySelector(".cancel");
-let body = document.querySelector("body");
-let mobileLinks = document.querySelectorAll(".nav-mobile");
-hamburger.addEventListener("click", () => {
-  mobileMenu.classList.add("show");
-  body.classList.add("overflowHidden");
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const cancel = document.querySelector('.cancel');
+const body = document.querySelector('body');
+const mobileLinks = document.querySelectorAll('.nav-mobile');
+hamburger.addEventListener('click', () => {
+  mobileMenu.classList.add('show');
+  body.classList.add('overflowHidden');
 });
-cancel.addEventListener("click", () => {
-  mobileMenu.classList.remove("show");
-  body.classList.remove("overflowHidden");
+cancel.addEventListener('click', () => {
+  mobileMenu.classList.remove('show');
+  body.classList.remove('overflowHidden');
 });
 
 mobileLinks.forEach((item) => {
-  item.addEventListener("click", () => {
-    mobileMenu.classList.remove("show");
-    body.classList.remove("overflowHidden");
+  item.addEventListener('click', () => {
+    mobileMenu.classList.remove('show');
+    body.classList.remove('overflowHidden');
   });
 });

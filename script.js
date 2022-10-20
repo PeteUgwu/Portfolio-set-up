90a84da0a321c7a2fed4826ead240430d699e108
peter <peterugwu64@gmail.com>
2022-10-20 09:26:51 +0100

    modified css, js and html files

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let modal = document.querySelector(".modal");
 let modalInner = document.querySelector(".modal-inner");
 let modalBtn = document.querySelectorAll(".btn-modal");
 let cancelsModal = document.querySelector(".cancels");
+let width = window.innerWidth;
+let isDesktop = width > 768;
 
 const data = [
   {
@@ -14,8 +16,11 @@ const data = [
     name: "Tonic",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    featuredImage: "./images/Snap.png",
-    technologies: ["html", "css", "javascript"],
+    desktopDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    featuredImage: "./images/Snapshoot1.png",
+    desktopFeaturedImage: "./images/Snap.png",
+    technologies: ["html", "css", "javascript", "github", "ruby", "Bootstraps"],
     linkToLiveVersion: "https://peteugwu.github.io/Portfolio-set-up/",
     linkToSource: "https://github.com/PeteUgwu/Portfolio-set-up",
   },
@@ -24,8 +29,11 @@ const data = [
     name: "Multi-Post Stories",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    featuredImage: "./images/Snap2.png",
-    technologies: ["html", "css", "javascript"],
+    desktopDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    featuredImage: "./images/Snapshoot2.png",
+    desktopFeaturedImage: "./images/Snap2.png",
+    technologies: ["html", "css", "javascript", "github", "ruby", "Bootstraps"],
     linkToLiveVersion: "https://peteugwu.github.io/Portfolio-set-up/",
     linkToSource: "https://github.com/PeteUgwu/Portfolio-set-up",
   },
@@ -34,8 +42,11 @@ const data = [
     name: "Facebook 360",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    featuredImage: "./images/Snapshoot1.png",
-    technologies: ["html", "css", "javascript"],
+    desktopDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    featuredImage: "./images/Snapshoot3.png",
+    desktopFeaturedImage: "./images/Snap3.png",
+    technologies: ["html", "css", "javascript", "github", "ruby", "Bootstraps"],
     linkToLiveVersion: "https://peteugwu.github.io/Portfolio-set-up/",
     linkToSource: "https://github.com/PeteUgwu/Portfolio-set-up",
   },
@@ -44,8 +55,11 @@ const data = [
     name: "Uber Navigation",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    featuredImage: "./images/Snap4.png",
-    technologies: ["html", "css", "javascript"],
+    desktopDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    featuredImage: "./images/Snapshoot4.png",
+    desktopFeaturedImage: "./images/Snap4.png",
+    technologies: ["html", "css", "javascript", "github", "ruby", "Bootstraps"],
     linkToLiveVersion: "https://peteugwu.github.io/Portfolio-set-up/",
     linkToSource: "https://github.com/PeteUgwu/Portfolio-set-up",
   },
@@ -54,7 +68,9 @@ const data = [
 const openModal = ({
   name,
   description,
+  desktopDescription,
   featuredImage,
+  desktopFeaturedImage,
   technologies,
   linkToLiveVersion,
   linkToSource,
@@ -63,29 +79,36 @@ const openModal = ({
   modal.classList.add("showModal");
   display += `
   <div>
-  <h2>${name}</h2>
-  <ul class="flex item-tag">
+  <h2 class="modal-h2">${name}</h2>
+  <ul class="flex item-tag-modal item-tag">
     <li class="icon-tag"><a class="canopy" href="#">CANOPY</a></li>
     <li class="radio icon-tag"><img src="images/Counter.png" alt="counter" /></li>
     <li class="icon-tag"><a class="bend-yr bend" href="#">Back End Dev</a></li>
     <li class=" radio icon-tag"><img src="images/Counter.png" alt="counter" /></li>
     <li class="icon-tag"><a class="bend-yr"href="#">2015</a></li>
   </ul>
-  <div class="image ">
-    <img src=${featuredImage} alt="Project"/>
+  <div class="image img-modal ">
+    <img src=${isDesktop ? desktopFeaturedImage : featuredImage} alt="Project"/>
   </div>
-  <div class="card">
-    <p class="p">
-     ${description}
+  <div class="flex-modal">
+    <p class="p p-modal">
+     ${isDesktop ? desktopDescription : description}
     </p>
-    <div class="langbutton">
+    
+    <div class="langbutton langbutton-modal">
       <button>${technologies[0]}</button>
       <button>${technologies[1]}</button>
       <button>${technologies[2]}</button>
+      ${isDesktop ? `<button>${technologies[3]}</button>` : ""}
+      ${isDesktop ? `<button>${technologies[4]}</button>` : ""}
+      ${isDesktop ? `<button>${technologies[5]}</button>` : ""}
+      
+      <div class="popup-buttons flex div">
+      <hr class="modal-new">
+      <a href=${linkToLiveVersion} target= "_blank" class="btn-modal btn">See Live <img src="./images/IconModal.png" alt="Live Icon " /></a>
+        <a href=${linkToSource} target= "_blank" class="btn-modal btn">See Source <img src="./images/VectorModal.png" alt="Source Icon" /></a>
     </div>
-    <div class="flex div">
-    <a href=${linkToLiveVersion} target= "_blank" class="btn-modal btn">See Live <img src="./images/IconModal.png" alt="twitter" /></a>
-      <a href=${linkToSource} target= "_blank" class="btn-modal btn">See Source <img src="./images/VectorModal.png" alt="twitter" /></a>
+    
     </div>
     </div>
 </div>`;
